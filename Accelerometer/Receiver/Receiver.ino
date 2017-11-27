24ac9df70a12ce1bf6028953a236b01b54359510
Vikas Patel <patel2vikas@gmail.com>
2017-11-27 22:26:43 +0530

    Receiver code added for FBRL
    pulse_time, pin 3,4,5,6

--- Accelerometer/Receiver/Receiver.ino
+++ Accelerometer/Receiver/Receiver.ino
@@ -1,9 +1,43 @@
+
+int F=3, B=4, R=5, L=6;
+char command=0;
+int pulse_time=10;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
+  pinMode(F, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(R, OUTPUT);
+  pinMode(L, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  command = Serial.read();
 
+  switch(command){
+    case 'F':
+       digitalWrite(F, HIGH);
+       delay(pulse_time);
+       digitalWrite(F, LOW);
+    break;
+    case 'B':
+       digitalWrite(B, HIGH);
+       delay(pulse_time);
+       digitalWrite(B, LOW);
+    break;
+    case 'R':
+       digitalWrite(R, HIGH);
+       delay(pulse_time);
+       digitalWrite(R, LOW);
+    break;
+    case 'L':
+       digitalWrite(L, HIGH);
+       delay(pulse_time);
+       digitalWrite(L, LOW);
+    break;
+  }
+  
 }
