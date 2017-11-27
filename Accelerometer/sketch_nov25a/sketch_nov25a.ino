a70edae29854e5bbc2405acc8878dad07a483680
Vikas Patel <patel2vikas@gmail.com>
2017-11-27 22:12:36 +0530

    Guidance and Control, Threshold set
    FBRL for movements
    Threshhold can be set in Run time

--- Accelerometer/sketch_nov25a/sketch_nov25a.ino
+++ Accelerometer/sketch_nov25a/sketch_nov25a.ino
@@ -1,5 +1,7 @@
-int X=A0, Y=A1, Z=A2;
+int X=A0, Y=A1, Z=A2, th_pin=2;
 int X_val=0, Y_val=0, Z_val=0;
+int X_th=0, Y_th=0;
+
 char buffer[50];
 
 void setup() {
@@ -8,6 +10,12 @@ void setup() {
   pinMode(X, INPUT);
   pinMode(Y, INPUT);
   pinMode(Z, INPUT);
+  pinMode(th_pin, INPUT);
+}
+
+void set_th(){
+  X_th = analogRead(X);
+  Y_th = analogRead(Y);
 }
 
 void loop() {
@@ -17,8 +25,26 @@ void loop() {
   Y_val = analogRead(Y);
   Z_val = analogRead(Z);
 
+  if(digitalRead(th_pin)){
+    set_th();
+  }
+
+  if(X_val - X_th > 30){
+  Serial.print("F");
+  }
+  if(X_val - X_th < -30){
+  Serial.print("B");
+  }
+
+  if(Y_val - Y_th > 30){
+  Serial.print("L");
+  }
+  if(Y_val - Y_th < -30){
+  Serial.print("R");
+  }
   
   sprintf(buffer, "%d  %d  %d \n", X_val, Y_val, Z_val);
-  Serial.print(buffer);
-  delay(1000);
+//  Serial.print(buffer);
+
+  delay(100);
 }
