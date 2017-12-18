bbbd5c736ae8c6063bc68a94a94aa1d6ade2fc7b
Vikas Patel <patel2vikas@gmail.com>
2017-12-18 20:26:14 +0530

    Threshold pin removed
    Threshold will set while reboot only
    Code tested for camera command and move command

--- Accelerometer/Transmitter/Transmitter.ino
+++ Accelerometer/Transmitter/Transmitter.ino
@@ -1,4 +1,4 @@
-int X=A0, Y=A1, Z=A2, th_pin=2;
+int X=A0, Y=A1, Z=A2, th_pin=2, cam_pin=3;
 int X_val=0, Y_val=0, Z_val=0;
 int X_th=0, Y_th=0;
 
@@ -11,6 +11,10 @@ void setup() {
   pinMode(Y, INPUT);
   pinMode(Z, INPUT);
   pinMode(th_pin, INPUT);
+  pinMode(cam_pin, INPUT);
+
+  set_th(); 
+  set_th(); 
 }
 
 void set_th(){
@@ -25,15 +29,23 @@ void loop() {
   Y_val = analogRead(Y);
   Z_val = analogRead(Z);
 
-  if(digitalRead(th_pin)){
-    set_th();
-  }
-
   if(X_val - X_th > 30){
-  Serial.print("F");
+    if(digitalRead(cam_pin)){
+      Serial.print("F");
+    }
+    else{
+      Serial.print("f");
+    }
   }
+  
   if(X_val - X_th < -30){
-  Serial.print("B");
+    
+    if(digitalRead(cam_pin)){
+     Serial.print("B");
+    }
+    else{
+     Serial.print("b");
+    }
   }
 
   if(Y_val - Y_th > 30){
@@ -43,7 +55,7 @@ void loop() {
   Serial.print("R");
   }
   
-  sprintf(buffer, "%d  %d  %d \n", X_val, Y_val, Z_val);
+  sprintf(buffer, "%d  %d  %d  %d\n", X_val, Y_val, X_th, Y_th);
 //  Serial.print(buffer);
 
   delay(100);
